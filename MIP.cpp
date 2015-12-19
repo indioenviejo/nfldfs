92bfdb80f7c403f473a906a8fdc5a7ba30192807
Raghav <raghav.padmanabhan@yandex.com>
2015-12-19 10:31:25 -0800

    Latest set of files as of 12/19

--- MIP.cpp
+++ MIP.cpp
@@ -31,22 +31,22 @@ int main(void)
   glp_add_rows(mip, 5);
   glp_set_row_name(mip, 1, "QB");
   glp_set_row_bnds(mip, 1, GLP_DB, 0.0, 1.0);
-  glp_set_row_name(mip, 2, "WR");
-  glp_set_row_bnds(mip, 2, GLP_DB, 0.0, 3.0);
+  glp_set_row_name(mip, 2, "RB");
+  glp_set_row_bnds(mip, 2, GLP_DB, 0.0, 2.0);
   glp_set_row_name(mip, 3, "TE");
-  glp_set_row_bnds(mip, 3, GLP_DB, 0.0, 2.0);
-  glp_set_row_name(mip, 4, "RB");
-  glp_set_row_bnds(mip, 4, GLP_DB, 0.0, 2.0);
+  glp_set_row_bnds(mip, 3, GLP_DB, 0.0, 1.0);
+  glp_set_row_name(mip, 4, "WR");
+  glp_set_row_bnds(mip, 4, GLP_DB, 0.0, 4.0);
   glp_set_row_name(mip, 5, "Cost");
-  glp_set_row_bnds(mip, 5, GLP_DB, 0.0, 46400.0);  
+  glp_set_row_bnds(mip, 5, GLP_DB, 0.0, 45800.0);  
   
-  int numPlayers = 398;
+  int numPlayers = 126;
   glp_add_cols(mip, numPlayers); // No. of players + 1
 
   FILE *myfile;
   double myvariable;
   int i;
-  myfile=fopen("/home/raghav/coeff.csv", "r");
+  myfile=fopen("/home/raghav/mygithub/nfldfs/week15coeff.csv", "r");
   for (i = 0 ; i < numPlayers; i++)
   {
     fscanf(myfile,"%lf",&myvariable);
@@ -60,12 +60,12 @@ int main(void)
   }
   
   
-    int ia[1+796], ja[1+796];
-    double ar[1+796];int j;
+    int ia[1+252], ja[1+252];
+    double ar[1+252];int j;
 
     //# Constraint 1 condition :QB
     int counter = 1;
-    for (j = 1 ; j<=55 ;++j)
+    for (j = 1 ; j<=33 ;++j)
     {
         ia[counter] = 1;
         ja[counter] = j;
@@ -74,7 +74,7 @@ int main(void)
     }
     
     //# Constraint 2 condition : RB
-    for (j = 56 ; j<=162 ;++j)
+    for (j = 34 ; j<=63 ;++j)
     {
         ia[counter] = 2;
         ja[counter] = j;
@@ -83,7 +83,7 @@ int main(void)
     }
 
     //# Constraint 3 condition : TE
-    for (j = 163 ; j<=251;++j)
+    for (j = 64 ; j<=76;++j)
     {
         ia[counter] = 3;
         ja[counter] = j;
@@ -92,18 +92,18 @@ int main(void)
     }
 
     //# Constraint 4 condition : WR
-    for (j = 252 ; j<=398 ;++j)
+    for (j = 77 ; j<=126 ;++j)
     {
         ia[counter] = 4;
         ja[counter] = j;
-        ar[counter] = myvariable;
+        ar[counter] = 1;
         counter = counter +1;
     }    
     
-  myfile=fopen("/home/raghav/salaries.csv", "r");
+  myfile=fopen("/home/raghav/mygithub/nfldfs/week15sals.csv", "r");
   printf("%d\n ",counter);
   printf("-------------------------\n");
-  for (j = 1 ; j <= 398; j++)
+  for (j = 1 ; j <= 126; j++)
   {
       fscanf(myfile,"%lf",&myvariable);
       printf("%.4f\n ",myvariable);
@@ -115,7 +115,7 @@ int main(void)
       printf("-------------------------\n");
   }    
     
-  glp_load_matrix(mip, 796, ia, ja, ar);
+  glp_load_matrix(mip, 252, ia, ja, ar);
 
   glp_iocp parm;
   glp_init_iocp(&parm);
@@ -125,7 +125,7 @@ int main(void)
 
   double z = glp_mip_obj_val(mip);
   printf("teamValue : %\lf\n  was selected",z);
-  for (j = 1 ; j<= 398 ; ++j)
+  for (j = 1 ; j<= 126 ; ++j)
   {
   double x1 = glp_mip_col_val(mip, j);
   if (x1 > 0)
--- main.py
+++ main.py
@@ -2,59 +2,58 @@
 import os
 import pandas as pda
 # 
-# root = "/home/raghav/mygithub/nfldfs/"
-# 
-# os.chdir(root)
-# 
-# import ComputationBlocks
-# 
-# #####################################################################################
-# season = 2015
-# weeks = range(12)
-# weeks = [a+1 for a in weeks]
-# #####################################################################################
-# 
-# # Read Salary and next game info Data
-# df = pda.read_csv(root+"DKSalaries_Week12.csv")
-# 
+root = "/home/raghav/mygithub/nfldfs/"
+ 
+os.chdir(root)
+ 
+import ComputationBlocks
+ 
+#####################################################################################
+season = 2015
+weeks = [7,8,9,10,11,12,13,14]
+#####################################################################################
+ 
+# Read Salary and next game info Data
+df = pda.read_csv(root+"DKSalaries.csv")
+ 
 # # Remove Defense List
 # dfOff = df[df["Position"] <> "DST"]
-# 
+#  
 # # Get the player statistics information
 # playerStats = ComputationBlocks.getPlayerStatsSummary(dfOff,season,weeks)
-# 
+#  
 # # Group the statistics by GSIS ID
 # players = playerStats.groupby(["GSIS_ID"])
-# 
+#  
 # playerStatsAway = pda.DataFrame()
 # playerStatsHME = pda.DataFrame()
-# 
+#  
 # # Get stats for each player at and away from home
 # for n,g in players:
 #     [tempAway,tempHME] =  ComputationBlocks.statsbyHomeFldAvd(g,"HomeFldAdv")    
 #     playerStatsAway = playerStatsAway.append(tempAway)
 #     playerStatsHME = playerStatsHME.append(tempHME)
-# 
+#  
 # print "----"
 # print playerStatsAway
 # print "----"
 # # Get average stats  @ home and away for each player 
 # playerAWYAvgPerf = ComputationBlocks.getPlayerAvgPerformance(playerStatsAway,"AWY")
 # playerHMEAvgPerf = ComputationBlocks.getPlayerAvgPerformance(playerStatsHME,"HME")
-# 
-# playerAWYAvgPerf.to_csv("/home/raghav/playerAway.csv")
-# playerHMEAvgPerf.to_csv("/home/raghav/playerHome.csv")
-# 
-# ###############################################################################################################
 #  
-# # Get Defense Teams
-# dfDef = df[df["Position"] == "DST"]
+# playerAWYAvgPerf.to_csv(root+"playerAwayMax.csv",index=None)
+# playerHMEAvgPerf.to_csv(root+"playerHomeMax.csv",index=None)
 # 
+###############################################################################################################
+  
+# Get Defense Teams
+# dfDef = df[df["Position"] == "DST"]
+#  
 # # Get the Stats for each defense team
 # defStats = ComputationBlocks.getDefStatsSummary(dfDef,season,weeks)
 # # Group the stats by team
 # defense =  defStats.groupby(["Team"])
-# 
+#  
 # defStatsAway = pda.DataFrame()
 # defStatsHME = pda.DataFrame()
 # # 
@@ -64,46 +63,46 @@ import pandas as pda
 #     [tempAway,tempHME] =  ComputationBlocks.statsbyHomeFldAvd(g,"HomeFldAdv")    
 #     defStatsAway = defStatsAway.append(tempAway)
 #     defStatsHME = defStatsHME.append(tempHME)
-# 
+#  
 # # ###############################################################################################################
 # # #Get Average Performance and normalize them
 # # ############################################################################################################### 
 # defAWYAvgPerf =  ComputationBlocks.getDefenseAvgPerformance(defStatsAway)
 # defHMEAvgPerf =  ComputationBlocks.getDefenseAvgPerformance(defStatsHME)
-# 
-# 
+#  
+#  
 # teamHMEAvgPerfNorm = ComputationBlocks.normalizeTeamStats(defHMEAvgPerf)
 # teamAWYAvgPerfNorm = ComputationBlocks.normalizeTeamStats(defAWYAvgPerf)
+ 
+ 
+teamHMEAvgPerfNorm = pda.read_csv(root+"teamNormHome.csv")
+teamAWYAvgPerfNorm= pda.read_csv(root+"teamNormAway.csv")
+playerAWYAvgPerf= pda.read_csv(root+"playerAway.csv")
+playerHMEAvgPerf= pda.read_csv(root+"playerHome.csv")
+ 
+ 
+# ############################################################################################################### 
+[away,home] = ComputationBlocks.getHomenAwy(df["GameInfo"])
 # 
 # 
-# teamHMEAvgPerfNorm.to_csv("/home/raghav/teamNormHome.csv")
-# teamAWYAvgPerfNorm.to_csv("/home/raghav/teamNormAway.csv")
-# 
-# 
-# # ############################################################################################################### 
-# [away,home] = ComputationBlocks.getHomenAwy(playerStats["NextGame"])
-# 
-# 
-# offensiveMatchup = ComputationBlocks.offMatchupPerformance (playerHMEAvgPerf,playerAWYAvgPerf,teamHMEAvgPerfNorm,teamAWYAvgPerfNorm,home,away)
+offensiveMatchup = ComputationBlocks.offMatchupPerformance (playerHMEAvgPerf,playerAWYAvgPerf,teamHMEAvgPerfNorm,teamAWYAvgPerfNorm,home,away)
 # 
 # print "-----------------------------------------"
 # print offensiveMatchup.head()
 # print "-----------------------------------------"
 # 
-# offensiveMatchup.to_csv("/home/raghav/offMatchup.csv",index=None)
+offensiveMatchup.to_csv(root+"offMatchup.csv",index=None)
 
 
 
 
 #############################################################################################################################################
 # Offensive Player Points
-x = pda.read_csv("/home/raghav/offMatchup.csv")
-
+x = pda.read_csv(root+"offMatchup.csv")
+# 
 points = []
 for i in range(x.shape[0]):
     points.extend([4*x.loc[x.index[i],"PassingTDs"]+0.04*x.loc[x.index[i],"PassingYards"]+3*max(0,min(1,x.loc[x.index[i],"PassingYards"]-300))-x.loc[x.index[i],"PassingInterceptions"]+0.1*x.loc[x.index[i],"RushingYards"]+6*x.loc[x.index[i],"RushingTDs"]+3*max(0,min(1,x.loc[x.index[i],"RushingYards"]-100))+0.1*x.loc[x.index[i],"ReceivingYards"]+6*x.loc[x.index[i],"ReceivingTDs"]+3*max(0,min(1,x.loc[x.index[i],"ReceivingYards"]-100))-x.loc[x.index[i],"FumblesLost"] + 2*(x.loc[x.index[i],"TwoPtsPassing"]+x.loc[x.index[i],"TwoPtsReceiving"]+x.loc[x.index[i],"TwoPtsRushing"])+6*x.loc[x.index[i],"OFRTD"]])
-
+ 
 x["Points"] = pda.Series(points)
-x.to_csv("/home/raghav/Wk13PlayerValue.csv")
-
-
+x.to_csv(root+"Wk15PlayerValue.csv")
